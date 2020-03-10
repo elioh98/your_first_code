f5d1da138ff850e3da98e9a0d2ef02a1209b0e65
Elio Haddad <eliohaddadd99@gmail.com>
2020-03-10 16:52:10 +0200

    updated main.c
    Requested version for the main.c code , and a shortened version of it

--- main.c
+++ main.c
@@ -1,9 +1,17 @@
 int main()
 {
   int i;
-  for (i = 0; i < 10; i++)
+  for (i = 0; i < 32; i++)
   {
-    printf("Holberton\n");
+    printf("Hello Holberton\n");
   }
   return 0;
 }
+-----------------------------
+int main()
+{
+    int i;
+    for(i=1;i<=32;printf("Hello Holberton\n",i++));
+    return 0;
+}
+/* shortened version */
